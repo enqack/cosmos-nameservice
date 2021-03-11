45c1606802c34fdd1313d75b06cb4ba20d5efcb5
enqack <enqack@gmail.com>
2021-03-11 15:40:53 -0500

    Added prices to whois actions.

--- x/nameservice/handler_whois.go
+++ x/nameservice/handler_whois.go
@@ -2,26 +2,59 @@ package nameservice
 
 import (
 	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 	"github.com/enqack/nameservice/x/nameservice/keeper"
 	"github.com/enqack/nameservice/x/nameservice/types"
 )
 
+var (
+	minimumCreateWhoisPrice, _ = types.CoinsFromString("10trycoin")
+	updateWhoisPrice, _        = types.CoinsFromString("5trycoin")
+	deleteWhoisPrice, _        = types.CoinsFromString("1trycoin")
+)
 
 func handleMsgCreateWhois(ctx sdk.Context, k keeper.Keeper, msg *types.MsgCreateWhois) (*sdk.Result, error) {
-	// Checks if whois name already exists
+	// Check if whois name already exists
 	if k.IsNamePresent(ctx, msg.Name) {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "name already exists")
 	}
-	// Checks is name is valid
+
+	// Check is name is valid
 	if !k.VerifyNameFormat(ctx, msg.Name) {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "name is not valid")
 	}
-	// Checks if address is valid
+
+	// Check if address is valid
 	if !k.IsValidAddress(ctx, msg.Address) {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "invalid address")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "address is not valid")
 	}
+
+	// Convert creator (type string) to sdk.AccAddress type
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert price (type string) to sdk.Coins type
+	price, err := types.CoinsFromString(msg.Price)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if price is above minimum
+	if minimumCreateWhoisPrice.IsAllGT(price) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "price below minimum")
+	}
+
+	// Deduct coins from creator's account
+	err = k.CoinKeeper.SubtractCoins(ctx, creator, price)
+	if err != nil {
+		return nil, err
+	}
+
 	k.CreateWhois(ctx, *msg)
 
 	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
@@ -36,14 +69,41 @@ func handleMsgUpdateWhois(ctx sdk.Context, k keeper.Keeper, msg *types.MsgUpdate
 		Price:   msg.Price,
 	}
 
-	// Checks that the element exists
+	// Check that the element exists
 	if !k.HasWhois(ctx, msg.Id) {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %s doesn't exist", msg.Id))
 	}
 
-	// Checks if the the msg sender is the same as the current owner
+	// Check if the the msg sender is the same as the current owner
 	if msg.Creator != k.GetWhoisOwner(ctx, msg.Id) {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+
+	// Check if whois name already exists
+	if k.IsNamePresent(ctx, msg.Name) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "name already exists")
+	}
+
+	// Check if name is valid
+	if !k.VerifyNameFormat(ctx, msg.Name) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "name is not valid")
+	}
+
+	// Check if address is valid
+	if !k.IsValidAddress(ctx, msg.Address) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "address is not valid")
+	}
+
+	// Convert creator (type string) to sdk.AccAddress type
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+
+	// Dedeuct coins from owner's account
+	err = k.CoinKeeper.SubtractCoins(ctx, creator, updateWhoisPrice)
+	if err != nil {
+		return nil, err
 	}
 
 	k.SetWhois(ctx, whois)
@@ -52,15 +112,29 @@ func handleMsgUpdateWhois(ctx sdk.Context, k keeper.Keeper, msg *types.MsgUpdate
 }
 
 func handleMsgDeleteWhois(ctx sdk.Context, k keeper.Keeper, msg *types.MsgDeleteWhois) (*sdk.Result, error) {
+	// Check if id exists
 	if !k.HasWhois(ctx, msg.Id) {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %s doesn't exist", msg.Id))
 	}
+
+	// Check if the the msg sender is the same as the current owner
 	if msg.Creator != k.GetWhoisOwner(ctx, msg.Id) {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+
+	// Convert creator (type string) to sdk.AccAddress type
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+
+	// Dedeuct coins from owner's account
+	err = k.CoinKeeper.SubtractCoins(ctx, creator, deleteWhoisPrice)
+	if err != nil {
+		return nil, err
 	}
 
 	k.DeleteWhois(ctx, msg.Id)
 
 	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
 }
-
--- x/nameservice/types/types.go
+++ x/nameservice/types/types.go
@@ -1,1 +1,30 @@
 package types
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+
+// CoinsFromString - convert price (type string) in the form of <amount><denom> to type sdk.Coins
+func CoinsFromString(price string) (sdk.Coins, error) {
+	re := regexp.MustCompile("[[:digit:]]+")
+	// get location of price amount
+	loc := re.FindIndex([]byte(price))
+	// get price amount as string
+	strAmount := re.FindString(price)
+	// get price demon from location of amount to end of price string 
+	denom := price[loc[1]:len(price)]
+	// convert to lower case
+	denom = strings.ToLower(denom)
+	// convert price amount from string to int64
+	amount, err := strconv.ParseInt(strAmount, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	// return Coins type
+	return sdk.NewCoins(sdk.NewInt64Coin(denom, amount)), nil
+}
